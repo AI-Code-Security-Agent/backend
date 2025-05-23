ca943fe4a9ba0e2223080ed612f1af80dd7e4178
Senevirathna12 <139707941+Senevirathna12@users.noreply.github.com>
2025-05-23 11:48:04 +0530

    initial commit

---
+++ config/send.email.config.js
@@ -0,0 +1,7 @@
+ module.exports.SEND_MAIL_CONFIG = {
+    service : 'gmail',
+    auth : {
+        user: 'testonenew31@gmail.com',
+        pass: 'ebty yizs ozbj zzis',
+    }
+ }
---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstname : {
+        type :String,
+        required :[true, "Please provide firstname.."]
+    },
+    lastname : {
+        type :String,
+        required :[true, "Please provide lastname.."]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    gitAccessTiken:{
+        type : String
+    }
+});
+
+const UserModel = mongoose.model('user',UserSchema);
+module.exports = UserModel;
