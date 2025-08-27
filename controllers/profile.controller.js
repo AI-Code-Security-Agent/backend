20b3b4022e10057b0d0c0c255a2c11f5ad1bebad
Senevirathna12 <139707941+Senevirathna12@users.noreply.github.com>
2025-08-27 22:24:19 +0530

    complete the personal info and password update features

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -5,7 +5,7 @@ const emailService = require("../email");
 const handleGetProfileData = async (req, res) => {
   try {
     const userId = req.user._id;
-    console.log("User ID :", userId);
+    // console.log("User ID :", userId);
     const user = await User.findById(userId);
 
     if (!user) {
@@ -33,6 +33,77 @@ const handleGetProfileData = async (req, res) => {
   }
 };
 
+const handleUpdatePersonalData = async (req, res) => {
+  try {
+   // const userId = req.user._id;
+    const { fullname, email } = req.body;
+
+    const user = await User.findOneAndUpdate(
+      { email },
+      { fullname, email },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({
+        isSuccess: false,
+        message: "User not found",
+        content: null,
+      });
+    }
+
+    res.status(200).json({
+      isSuccess: true,
+      message: "User updated successfully",
+      content: user,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      isSuccess: false,
+      message: "Internal server error",
+      content: null,
+    });
+  }
+};
+
+const handleUpdatePassword = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { newPassword } = req.body;
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({
+                isSuccess: false,
+                message: "User not found",
+                content: null,
+            });
+        }
+
+        const hashedNewPassword = await bcryptjs.hash(newPassword, 10);
+
+        user.password = hashedNewPassword;
+        await user.save();
+
+        res.status(200).json({
+            isSuccess: true,
+            message: "Password updated successfully",
+            content: null,
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            isSuccess: false,
+            message: "Internal server error",
+            content: null,
+        });
+    }
+};
+
 module.exports = {
-  handleGetProfileData
+  handleGetProfileData,
+  handleUpdatePersonalData,
+  handleUpdatePassword
 };
--- routes/profile.route.js
+++ routes/profile.route.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const profileController = require("../controllers/profile.controller");
 const { authenticateToken } = require("../auth/authToken");
 
-router.get(
-  "/profile_data",
-  authenticateToken,
-  profileController.handleGetProfileData
-);
+router.get("/profile_data",authenticateToken,profileController.handleGetProfileData);
+router.post("/update_personal_data",authenticateToken,profileController.handleUpdatePersonalData);
+router.post("/update_password",authenticateToken,profileController.handleUpdatePassword);
+  
+
 
 module.exports = router;
